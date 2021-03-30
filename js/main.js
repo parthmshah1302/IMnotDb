d3430632f3aa4cdbf440febe7da54dc3a2adc2ac
Parth Shah <parthmshah1302@gmail.com>
2021-03-31 02:15:08 +0530

    cdn for all

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ function getMovie(){
             ${movie.Plot}
             <hr>
             <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-            <a href="index.html" class="btn btn-default">Go Back To Search</a>
+            <a href="https://cdn.jsdelivr.net/gh/parthmshah1302/IMnotDb/index.html" class="btn btn-default">Go Back To Search</a>
           </div>
         </div>
       `;
